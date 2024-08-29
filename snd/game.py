1596e779b62af325168f1123c7774868ea16254e
Long17369 <long17369@foxmail.com>
2024-08-29 19:17:39 +0800

    新增了第一次点击防死机制（暂时没用） 新增了空白一键打开 新增了单元格类型类的All 新增了标旗（可显示） 新增了单元格数字显示 添加了部分类属性的类型定义 更改了鼠标点击逻辑 更改了单元格的绘画逻辑 更改了部分类属性的定义时间 对部分文件进行了格式化 对部分文件添加了注释 删除了部分print( )

--- snd/game.py
+++ snd/game.py
@@ -5,7 +5,7 @@ from typing import Tuple
 import pygame
 import time
 import snd.color as color
-from snd.mine import Cell,Cell_Type
+from snd.mine import Cell, Cell_Type
 import snd.constant as constant
 Level = constant.Level
 import random
@@ -15,42 +15,76 @@ class Game():
     """游戏逻辑
     """
 
-    def __init__(self, level:Level = Level()):
+    end: bool = False
+    run: bool = False
+
+    def __init__(self, level: Level = Level()):
         """初始化游戏
         """
         self.level = level
-        self.end = False
         self.screen = pygame.display.set_mode(
             (self.level.x * 25 + 40, self.level.y * 25 + 100))
         self.screen.fill(color.Gray_X11)
-        self.run = False
-        self.no_pos = Cell(-1,-1, self.screen)
+        self.no_pos = Cell(-1, -1, self.screen)
         self.mouse_motion_pos = self.no_pos
         self.minefield_surface_rect = constant.MINEFIELD_SURFACE_RECT
-        self.minefield_surface_border = pygame.Surface((self.level.x * 25+2, self.level.y * 25+2),flags=pygame.SRCALPHA)
+        self.minefield_surface_border = pygame.Surface(
+            (self.level.x * 25+2, self.level.y * 25+2), flags=pygame.SRCALPHA)
         self.minefield_surface = pygame.Surface(
             (self.level.x * 25, self.level.y * 25))
         self.minefield = [[Cell(x, y, self.minefield_surface)for y in range(
             self.level.y)] for x in range(self.level.x)]
         self.reflash()
-        [i.get_rect(self.minefield_surface_rect) for j in self.minefield for i in j]
+        [i.get_rect(self.minefield_surface_rect)
+         for j in self.minefield for i in j]
 
     def run_game(self):
         """开始一轮游戏
         """
         self.set_mine()
         self.time = time.time()
-        self.Timer = 0
+        self.Timer: int = 0
         self.run = True
         ...
 
-    def mouse_click(self, event):
+    def mouse_click(self, event: pygame.event.Event):
         """鼠标点击事件
         """
         if self.minefield_surface.get_rect(topleft=self.minefield_surface_rect).collidepoint(event.pos):
             if (not self.run) and event.button == 1:
                 self.run_game()
-            self.mouse_motion_pos.mouse_click(event)
+                if self.click_cell(event, self.mouse_motion_pos):
+                    self.run = False
+                    self.restart(event)
+            else:
+                self.end = self.click_cell(event, self.mouse_motion_pos)
+
+    def restart(self, event: pygame.event.Event):
+        """重置地雷位置
+        """
+        def reset(n: Cell):
+            n.number -= 1
+        [reset(i) for j in self.minefield for i in j if i.number in Cell_Type.Mine]
+        print('restart')
+        self.mouse_click(event)
+
+    def click_cell(self, event, pos: Cell):
+        if pos.mouse_click(event):
+            return True
+        if event.button == 1:
+            if pos.number == 0:
+                x = pos.pos[0]
+                y = pos.pos[1]
+                for i in range(x-1, x+2):
+                    for j in range(y-1, y+2):
+                        try:
+                            posTemp = self.minefield[i][j]
+                            if posTemp.type in Cell_Type.Unexplored:
+                                if self.click_cell(event, self.minefield[i][j]):
+                                    raise SystemError('Game Error')
+                        except IndexError:
+                            ...
+        return False
 
     def mouse_motion(self, event: pygame.event.Event):
         """鼠标移动事件
@@ -79,9 +113,11 @@ class Game():
         """重绘游戏界面
         """
         self.screen.fill(color.Gray_X11)
-        Rect = (0,0,self.minefield_surface_border.get_width(),self.minefield_surface_border.get_height())
+        Rect = (0, 0, self.minefield_surface_border.get_width(),
+                self.minefield_surface_border.get_height())
         rect = (i-1 for i in self.minefield_surface_rect)
-        pygame.draw.rect(self.minefield_surface_border, (0,0,0,156), Rect, 1)
+        pygame.draw.rect(self.minefield_surface_border,
+                         (0, 0, 0, 156), Rect, 1)
         self.minefield_surface.fill(color.Gray_X11)
         [i.reflash() for j in self.minefield for i in j]
         self.screen.blit(self.minefield_surface_border, tuple(rect))
@@ -93,14 +129,14 @@ class Game():
 
         mine_count = 0
         while mine_count <= self.level.mine_count:
-            x = random.randint(0,self.level.x-1)
-            y = random.randint(0,self.level.y-1)
+            x = random.randint(0, self.level.x-1)
+            y = random.randint(0, self.level.y-1)
             if self.minefield[x][y].type not in Cell_Type.Mine:
                 self.minefield[x][y].type = Cell_Type.Mine_Unexplored
-                print((x,y))
+                # print((x, y))
                 mine_count += 1
-                for i in range(x-1,x+2):
-                    for j in range(y-1,y+2):
+                for i in range(x-1, x+2):
+                    for j in range(y-1, y+2):
                         try:
                             self.minefield[i][j].number += 1
                         except IndexError:
--- snd/mine.py
+++ snd/mine.py
@@ -6,84 +6,183 @@ import snd.color as color
 
 
 class Cell_Type():
+    # 定义单元格类型类，用于表示扫雷游戏中的各种单元格状态
     Safe_Unexplored = 0     # 未挖 安全
     Mine_Unexplored = 1     # 未挖 地雷
     Safe_Flag = 2           # 标记 安全
     Mine_Flag = 3           # 标记 地雷
     Safe_explored = 4       # 已挖 安全
-    Mine_explored = 5       # 已挖 地雷
-    Safe = {Safe_Unexplored, Safe_Flag, Safe_explored}
-    Mine = {Mine_Unexplored, Mine_Flag, Mine_explored}
-    Unexplored = {Safe_Unexplored, Mine_Unexplored}
-    explored = {Safe_explored, Mine_explored}
-    Flag = {Safe_Flag, Mine_Flag}
-    No_Flag = {Safe_Unexplored, Safe_explored, Mine_Unexplored, Mine_explored}
+    Mine_explored = 5       # 已挖 地雷}
+    Safe = {Safe_Unexplored, Safe_Flag, Safe_explored}      # 安全的单元格集合
+    Mine = {Mine_Unexplored, Mine_Flag, Mine_explored}      # 地雷的单元格集合
+    All = Safe | Mine                                       # 所有的单元格集合
+    Unexplored = {Safe_Unexplored, Mine_Unexplored}         # 未探索的单元格集合
+    explored = {Safe_explored, Mine_explored}               # 已探索的单元格集合
+    Flag = {Safe_Flag, Mine_Flag}                           # 标记的单元格集合
+    No_Flag = All - Flag                                    # 未标记的单元格集合
 
 
 class Cell():
     """格子
     """
 
-    def __init__(self, x: int, y: int, game_surface: pygame.Surface, size=25):
+    number: int = 0
+    mouse: bool = False
+    type = Cell_Type.Safe_Unexplored
+
+    def __init__(self, x: int, y: int, game_surface: pygame.Surface, size: int = 25):
+        """
+        初始化棋子对象。
+
+        x: 棋子的x坐标
+        y: 棋子的y坐标
+        game_surface: 棋子所在的pygame Surface对象
+        size: 棋子的大小，默认为25
+        """
+        # 特殊情况处理：如果x和y都为-1，则表示这个棋子代表雷区之外的区域
         if x == -1 and y == -1:
             self.x = -1
             self.y = -1
             self.mouse = True
             return
+
+        # 初始化棋子的坐标、大小和相关属性
         self.x = x
         self.y = y
         self.size = size
         self.pos = (x, y)
-        self.Rect_x = x*size
-        self.Rect_y = y*size
-        self.Rect = (x*size, y*size, size, size)
-        self.mouse = False
+        self.Rect_x = x * size
+        self.Rect_y = y * size
+        # 定义棋子在游戏界面中的矩形区域
+        self.Rect = (x * size, y * size, size, size)
+        # 保存游戏界面的Surface对象
         self.game_surface = game_surface
-        self.type = Cell_Type.Safe_Unexplored
-        self.number = 0
+        # 初始化字体对象，用于在棋子上显示文字或数字
+        self.font = pygame.font.Font(None, self.size)
+        # 初始化棋子的绘制参数，包括颜色和图形等
+        self.cell_list = [
+            (255, 255, 255, 128),
+            [(0, 0), (size, 0), (0, size)],
+            (0, 0, 0, 64),
+            [(size, size), (size, 0), (0, size)],
+            (3, 3, size - 6, size - 6)
+        ]
 
     def get_rect(self, topleft):
+        """获取棋子的矩形区域"""
         self.rect = self.surface.get_rect(
             topleft=(topleft[0]+self.Rect_x, topleft[1]+self.Rect_y))
 
     def mouse_click(self, event: pygame.event.Event):
+        """
+        处理鼠标点击事件。
+
+        根据不同的鼠标按钮和当前单元格状态，更改单元格的类型。
+        支持左键点击和右键点击操作，用于标记和取消标记单元格。
+
+        参数:
+        event 包含事件信息
+
+        返回:
+        如果点击的单元格是隐藏的雷，则返回True，否则返回False。
+        """
         if event.type == pygame.MOUSEBUTTONDOWN:
+            # 左键点击且单元格为未探索状态，探雷
             if event.button == 1 and self.type in Cell_Type.Unexplored:
-                print(self.type, end=' ')
-                print(self.number)
                 self.type += 4
+            # 左键点击已标记为旗子的单元格，无动作
             elif event.button == 1 and self.type in Cell_Type.Flag:
                 ...
+            # 点击已探索的单元格，无动作
+            elif self.type in Cell_Type.explored:
+                ...
+            # 右键点击标记为旗子的单元格，将其类型改为未探索
             elif event.button == 3 and self.type in Cell_Type.Flag:
                 self.type -= 2
+            # 右键点击未探索的单元格，将其标记为旗子
             elif event.button == 3 and self.type in Cell_Type.Unexplored:
                 self.type += 2
             else:
+                # 出现未知的鼠标点击情况，打印错误信息和点击按钮及单元格类型
                 print('error')
                 print(event.button)
                 print(self.type)
+        # 返回点击的单元格是否为探查过的雷
+        return self.type == Cell_Type.Mine_explored
 
     def reflash(self):
-        x = self.x
-        y = self.y
+        # 根据当前单元格的大小创建一个透明的Surface
         size = self.size
         self.surface = pygame.Surface((size, size), flags=pygame.SRCALPHA)
-        if self.type in Cell_Type.Unexplored | Cell_Type.Flag:
-            pygame.draw.polygon(self.surface, (255, 255, 255, 128), [
-                                (0, 0), (size, 0), (0, size)])
-            pygame.draw.polygon(self.surface, (0, 0, 0, 64), [
-                                (size, size), (size, 0), (0, size)])
-            self.game_surface.blit(self.surface, (self.Rect_x, self.Rect_y))
-            pygame.draw.rect(self.game_surface, color.Silver,
-                             (x*size+3, y*size+3, size-6, size-6))
-            if self.mouse:
-                a = pygame.Surface((size, size), flags=pygame.SRCALPHA)
-                pygame.draw.rect(a, (255, 255, 255, 128), (0, 0, size, size))
-                self.game_surface.blit(a, (self.Rect_x, self.Rect_y))
-        if self.type in Cell_Type.explored:
-            pygame.draw.rect(self.surface, (255, 255, 255, 128),
-                             (self.Rect_x, self.Rect_y, size, size), 1)
-            if self.type == Cell_Type.Safe_explored:
-                ...
+        # 为Surface添加一个黑色边缘
+        pygame.draw.rect(self.surface, (0, 0, 0, 64), (0, 0, size, size), 1)
 
-            self.game_surface.blit(self.surface, (self.Rect_x, self.Rect_y))
+        # 如果当前单元格未探索或被标记
+        if self.type in Cell_Type.Unexplored | Cell_Type.Flag:
+            # 绘制单元格本身
+            self.draw_cell()
+            # 如果鼠标在单元格上，绘制高亮效果
+            if self.mouse:
+                self.draw_highlight()
+            # 如果单元格被标记，绘制旗帜
+            if self.type in Cell_Type.Flag:
+                self.draw_cell()
+                self.draw_flag()
+
+        # 如果当前单元格是已探索类型
+        if self.type in Cell_Type.explored:
+            # 如果单元格周围有雷，绘制数字
+            if self.number != 0:
+                self.draw_number()
+
+        # 将Surface内容绘制到游戏主界面上
+        self.game_surface.blit(self.surface, (self.Rect_x, self.Rect_y))
+
+    def draw_cell(self):
+        """
+        绘制格子
+
+        此函数负责在图形界面上绘制一个细胞单元的形状和颜色。使用pygame库的绘制功能，
+        通过self.cell_list存储的顶点信息来绘制阴影，并绘制格子本身。
+        """
+        # 获取绘制信息
+        polygon = self.cell_list
+
+        # 绘制阴影
+        pygame.draw.polygon(self.surface, polygon[0], polygon[1])
+        pygame.draw.polygon(self.surface, polygon[2], polygon[3])
+        # 绘制格子
+        pygame.draw.rect(self.surface, color.Silver, polygon[4])
+
+    def draw_flag(self):
+        """绘制旗帜"""
+        size = self.size
+        pygame.draw.polygon(self.surface, color.Red, [
+                            (size/2, size/6), (size/2, size/2), (size/5, size/3)])
+        pygame.draw.line(self.surface, color.Black,
+                         (size/2, size/2), (size/2, size*0.7), int(size//25))
+        pygame.draw.line(self.surface, color.Black, (size*0.25,
+                         size*0.7), (size*0.75, size*0.7), int(size//25*2))
+
+    def draw_highlight(self):
+        """
+        绘制高亮区域
+
+        该方法用于在游戏界面中绘制一个半透明的白色高亮效果，用于突出显示某个区域。
+        它通过创建一个带有SRCALPHA标志的Surface对象来实现半透明效果，然后在这个
+        Surface对象上绘制一个白色的矩形，最后将这个Surface对象与游戏界面合并，达到
+        高亮显示的效果。
+        """
+        size = self.size
+        # 创建一个带有SRCALPHA标志的Surface对象，用于支持透明度设置
+        a = pygame.Surface((size, size), flags=pygame.SRCALPHA)
+        # 在Surface对象上绘制一个半透明的白色矩形
+        pygame.draw.rect(a, (255, 255, 255, 128), (0, 0, size, size))
+        # 将带有高亮效果的Surface对象绘制到游戏界面上
+        self.surface.blit(a, (0,0))
+
+    def draw_number(self):
+        number = self.font.render(str(self.number), True, color.Black)
+        number_rect = number.get_rect()
+        number_rect.center = self.surface.get_rect().center
+        self.surface.blit(number, number_rect)
